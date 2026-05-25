d4ffc89fe0ce1197c8d0d5a82d282be48dc31149
Leonardo Custodio <leonardo@custodio.me>
2026-05-25 17:17:38 -0300

    Bumping to host 0.8.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ import { useAccounts } from "@/src/lib/use-accounts";
 import { useConnectionStatus } from "@/src/lib/use-connection-status";
 import { stringify } from "@/src/lib/utils";
 
-const SDK_VERSION = "0.7.9";
+const SDK_VERSION = "0.8.0-1";
 
 const categoryIcons: Record<TestCategory, string> = {
   extension: "🔌",
